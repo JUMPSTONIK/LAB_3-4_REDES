fc20d6b0eabaa1aae6c9368f8c5edee928379a15
JUMPSTONIK <xtineen@gmail.com>
2020-09-05 18:38:41 -0600

    creando el controller y añadiendo el algoritmo 3

--- controller.py
+++ controller.py
@@ -1,3 +1,47 @@
+import socket
+import threading
+import pickle
+HEADERSIZE = 10
+typeEncode = 'utf-8'
+
+nickname = input("choose a nickname: ")
+
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 55555))
+
+def  recieve():
+    while True:
+        try:
+            message = client.recv(1024).decode(typeEncode)
+            if message == 'NICK':
+                client.send(nickname.encode(typeEncode))
+            else:
+                print(message)
+        except:
+            print("an error ocurred!")
+            client.close()
+            break
+
+def write():
+    while True:
+        message = f'{nickname}: {input("")}'
+        client.send(message.encode(typeEncode))
+
+recieve_thread = threading.Thread(target=recieve)
+recieve_thread.start()
+
+#con este podemos enviar objetos a otros clientes
+def send_obj(o):
+    obj = pickle.dumps(o)
+    obj = bytes(f"{len(obj):<{HEADERSIZE}}", 'utf-8') + obj
+    client.send(obj)
+
+#con esta funcion podemos recibir objetos que nos manden
+def recv_obj():
+    obj = client.recv(1024)
+    data = pickle.loads(obj[HEADERSIZE:])
+    return data
+
 graph = {'a':{'b':10,'c':3},'b':{'c':1,'d':2},'c':{'b':4,'d':8,'e':2},'d':{'e':7},'e':{'d':9}}
  
 def dijkstra(graph,start,goal):
@@ -41,4 +85,7 @@ def dijkstra(graph,start,goal):
         return path
 
  
-dijkstra(graph, 'a', 'd')
+#dijkstra(graph, 'a', 'd')
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
