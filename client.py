69bc9963ac255974894e94d627b2b5ac96685358
Luis <ledodo99@hotmail.com>
2020-09-05 22:43:18 -0600

    Funcionalidad de client

--- client.py
+++ client.py
@@ -1,24 +1,81 @@
 import socket
 import threading
 import pickle
+from modelos import paquete
+import time
+import traceback
+
 HEADERSIZE = 10
 typeEncode = 'utf-8'
-
-nickname = input("choose a nickname: ")
+neighborNames=[]
+neighborCosts=[]
+ip=input("Ingrese la dirección para conectarse: ")
+port=int(input("Ingrese el numero de puerto: "))
+nickname = input("Choose a nickname: ")
 
 client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 55555))
+client.connect((ip, port))
+
+def send_message(mensaje,neighborNum):
+    global neighborNames
+    global neighborCosts
+    global nickname
+    nodes=mensaje.get_pastNode()
+    nodes.append(nickname)
+    mensaje.set_nextNode(neighborNames[neighborNum])
+    mensaje.set_sendingNode(nickname)
+    mensaje.set_distance(mensaje.get_distance()+neighborCosts[neighborNum])
+    mensaje.set_jumps(mensaje.get_jumps()+1)
+    mensaje.set_pastNode(nodes)
+    send_obj(mensaje)
+
+def send_flood(mensaje):
+    global neighborNames
+    global neighborCosts
+    for i in range(len(neighborNames)):
+        if (neighborNames[i] != mensaje.get_sendingNode()):
+            send_message(mensaje,i)
+
+def send_directed(mensaje):
+    global neighborCosts
+    global neighborNames
+    global nickname
+    pathASeguir = mensaje.get_path()
+    pos=pathASeguir.index(nickname)+1
+    send_message(mensaje,pos)
+
+def print_message_recieved(mensaje):
+    print("Nodo origen: "+mensaje.get_orgigin())
+    print("Nodo destino: "+mensaje.get_goal())
+    print("Mensaje: "+mensaje.get_msg())
+    print("Nodos pasados: ")
+    print(mensaje.get_pastNode())
+    print("Saltos: "+str(mensaje.get_jumps()))
+    print("Distancia recorrida: "+str(mensaje.get_distance()))
 
 def  recieve():
     while True:
         try:
-            message = client.recv(1024).decode(typeEncode)
-            if message == 'NICK':
+            message = client.recv(1024)
+            a=message.decode(typeEncode)
+            if a == 'NICK':
+                print("Se hizo el envio del nick")
                 client.send(nickname.encode(typeEncode))
             else:
-                print(message)
+                print("Se recibio un pickle")
+                mensajeReciv=pickle.loads(message[HEADERSIZE:])
+                print("Se decodificó")
+                print(mensajeReciv)
+                if (mensajeReciv.get_goal()!=username):
+                    if(mensajeReciv.get_algorithm()==1):
+                        if (mensajeReciv.get_jumps()<mensajeReciv.get_maxJumps()):
+                            send_flood(mensajeReciv)
+                    else:
+                        send_directed(mensajeReciv)
+                print_message_recieved(mensajeReciv)
         except:
             print("an error ocurred!")
+            traceback.print_exc()
             client.close()
             break
 
